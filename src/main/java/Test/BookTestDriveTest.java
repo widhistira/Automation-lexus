b703671e84cc73d93e14b9c382c0789ff74bc66f
unknown <widhistira79@gmail.com>
2024-07-02 23:51:14 +0900

    basic code open web automation

--- src/main/java/Test/BookTestDriveTest.java
+++ src/main/java/Test/BookTestDriveTest.java
@@ -1,6 +1,6 @@
 package Test;
 
-import Page.BookPage;
+import Page.inputPage;
 import Page.OpenPage;
 //import com.sun.org.apache.bcel.internal.classfile.Utility;
 import Utility.Utility;
@@ -18,65 +18,8 @@ public class BookTestDriveTest extends BaseTest {
 
     }
 
-    @Test(testName = "BOOK A TEST DRIVE OR APPOINTMENT ")
+    @Test(testName = "Basic open google")
     public void BookADrive() throws InterruptedException {
-        BookPage bookPage = new BookPage(BaseTest.driver,BaseTest.explicitWait);
-        Page.ScrollPage scrollPage = new Page.ScrollPage(driver,explicitWait);
-
-        Thread.sleep(1000);
-
-        bookPage.acceptcookies();
-        bookPage.inptFirstName("Ryan");
-        Thread.sleep(1000);
-        bookPage.inptLastName("Widhistira");
-        Thread.sleep(1000);
-        bookPage.inptEmailAddress("widhistira79@gmail.com");
-        Thread.sleep(1000);
-        bookPage.clickPhoneDropDown();
-        bookPage.selectPhoneRegion();
-        Thread.sleep(1000);
-        bookPage.InputPhoneNum("91234567");
-        scrollPage.scrollDownByPixel();
-        Thread.sleep(1000);
-        bookPage.selectDate();
-        bookPage.pickDate();
-        Thread.sleep(1000);
-        bookPage.clickPreferedTime();
-        bookPage.selectTime();
-//        scrollPage.scrollDownByPixel();
-        Thread.sleep(1000);
-        bookPage.clickPreferedSales();
-        bookPage.selectSales();
-        Thread.sleep(1000);
-        scrollPage.scrollDownByPixel();
-        bookPage.clickPax();
-        bookPage.selectPax();
-        Thread.sleep(1000);
-        Assert.assertTrue(bookPage.BtnSubmitDisable());
-//        Utility.hardWait(2);
-        Thread.sleep(1000);
-        Assert.assertTrue(bookPage.verifyModel());
-        bookPage.deleteModel();
-        bookPage.clickPreferedModel();
-        bookPage.selectModelPrefered();
-        bookPage.clickTestdriveDropBtn();
-        bookPage.selectModel();
-        Thread.sleep(1000);
-    //        bookPage.inputYearVehicle("1");
-    //        Thread.sleep(1000);
-        bookPage.selectTestDriveOption();
-        Thread.sleep(1000);
-        bookPage.inputVehicleModel("UX 300e");
-        Thread.sleep(1000);
-        bookPage.inputYearVehicle("2020");
-        Thread.sleep(1000);
-        bookPage.checkboxDriveLicense();
-        bookPage.checkboxTermCondition();
-        scrollPage.scrollDownByPixel();
-//        bookPage.clickCheckBox();
-        bookPage.clickCheckBoxMarketing();
-//        bookPage.scrollUntilBtnSbmit();
-        Thread.sleep(2000);
 
     }
 
